37dabc7b9f08b1d0785aaed8e0b76f26b9ba78ad
Nayeem_XTREME <nayeemxtreme@live.com>
2020-10-03 15:18:39 +0600

    Tablet Done

---
+++ js/resources/script.js
@@ -0,0 +1,28 @@
+// $(document).ready(function() {
+//     // Sticky Nav
+//     $('.js--content').waypoint(function(direction) {
+//         if (direction == "down") {
+//             $('.header').addClass('sticky');
+//         } else {
+//             $('.header').removeClass('sticky');
+//         }
+//     }, {
+//         offset: '60px'
+//     });
+// })
+
+// $(document).ready(function() {
+//     $('.js--content').click(function(){
+//         $('.header').addClass('sticky');
+//     });
+// })
+
+$(document).ready(function() {
+    $(window).scroll(function() {
+      if ($(document).scrollTop() > 650) {
+        $(".header").addClass("sticky");
+      } else {
+        $(".header").removeClass("sticky");
+      }
+    });
+});
