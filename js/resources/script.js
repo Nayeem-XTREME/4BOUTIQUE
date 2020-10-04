7985e6213e56ed161f22ff8016ad2547e8861341
Nayeem_XTREME <nayeemxtreme@live.com>
2020-10-04 11:25:43 +0600

    Tablet Portrait added

--- js/resources/script.js
+++ js/resources/script.js
@@ -12,7 +12,7 @@ $(document).ready(function() {
       $('html, body').animate({
         scrollTop: $(hash).offset().top
       }, 800, function(){
-
+        window.location.hash = hash;
       });
     }
 
