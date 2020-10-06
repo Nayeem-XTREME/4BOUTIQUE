31973aeda2c784dd58d4a58a244a30c3aad2322a
Nayeem_XTREME <nayeemxtreme@live.com>
2020-10-06 21:52:39 +0600

    Sidebar + Slider (Raisa)

--- js/resources/script.js
+++ js/resources/script.js
@@ -30,6 +30,11 @@ $(document).ready(function() {
   
 });
 
+
+
+
+
+
 // Fullscreen Sidebar (Raisa)
 $(function() {
   $('#ham').on('click', function() {
@@ -47,3 +52,41 @@ $(function() {
     $('#ham-nav').toggleClass('cross-screen');
   });
 });
+
+// Slider (Raisa)
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var img = ['../img/latest-autumn-fashion-trends-for-women-wallpaper-preview.jpg','../img/mens-fashion-trends-2020.jpg',
+              '../img/people-man-travel-fashion.jpg','../img/mens-fashion-trends-2020.jpg',
+              '../img/hugo-boss-black-ss-2012-mark-vanderloo-3.jpg'];
+
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  var home = document.getElementById("home-id");
+
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  slideIndex++;
+
+  if (slideIndex > slides.length) {
+    slideIndex = 1
+  }   
+
+  imgs = img.shift();
+  img.push(imgs);
+
+  home.style.backgroundImage='url(' + img[slideIndex-1] + ')';
+
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 2000);
+}
